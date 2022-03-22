8049c2b3c7c484b22aa8af3a7b072528b42f7cbc
rhuancoder <rsjlcarvalho@gmail.com>
2022-03-21 22:36:53 -0300

    Adding Trucks.Web project

---
+++ Trucks.Web/Controllers/TruckController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trucks.Web.Controllers
+{
+    public class TruckController : Controller
+    {
+        public TruckController()
+        {
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var trucks = new List<Models.TruckViewModel>()
+            {
+                new Models.TruckViewModel() 
+                { 
+                    Description = "Xpto",
+                    TruckModel = "FM",
+                    ManufactureYear = 2019,
+                    ModelYear = 2022
+                }
+            };
+
+            return View(trucks);
+        }
+    }
+}
---
+++ Trucks.Web/Models/TruckViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace Trucks.Web.Models
+{
+    public class TruckViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Truck Model")]
+        public string TruckModel { get; set; }
+
+        [DisplayName("Manufacture Year")]
+        public int ManufactureYear { get; set; }
+
+        [DisplayName("Model Year")]
+        public int ModelYear { get; set; }
+    }
+}
